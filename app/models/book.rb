241e7cbf16353b77744e9673475aa91c854cfdc3
EC2 Default User <ec2-user@ip-172-31-16-103.ap-northeast-1.compute.internal>
2023-06-25 15:30:58 +0000

    新規投稿

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
     
     has_one_attached :image
-    belpnge_to :user
+    belongs_to :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
+  post "books" => "books#create"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
